7bafb576efdac34163e630ae00b77316f7670b84
Ahmed Yasser <ahmedyassermohammad@gmail.com>
2023-08-17 02:07:47 +0000

    adding solution of task5

---
+++ day5/demo/script.js
@@ -0,0 +1,59 @@
+// function sum (x,y){
+//     // x=x||2;
+//     // x=x?x:2
+//     // y=y||4;
+//     y=y?y:4
+
+//     return x + y;
+// }
+// console.log(sum())   
+
+// var a =20;
+// // console.log(myFun(6,7))
+// function myFun(x,y){
+//     console.log(z)
+//     var z=10;
+//     b=2;
+//     return a+x+y+z+b;
+// }
+// // console.log(a)
+// // // console.log(b)
+//  console.log(myFun(1,2)) //35
+
+// console.log(b)
+
+//     console.log(a)
+
+
+
+
+
+var str="this Is javaScript string object demo"
+console.log(str.replace("i","_"))
+// /pattern/ flag
+//g ==>global
+//m ==>multiple
+//i ==>ignore sensitivity
+// var regEx=/i/gi
+// console.log(str.replace(regEx,"_"))
+// [A-Z][a-z]{min,max}
+// [0-9]{2,5}
+// [0-9]{,5}
+// [0-9]{2,}
+// [0-9]{5}
+
+var val=prompt();
+var regExp=/^[0-9]{8}$/
+
+console.log(regExp.test(val));
+
+
+
+
+
+
+
+
+
+
+
